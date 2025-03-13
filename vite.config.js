4868716fd2f427632134354941bf9770dc15de4b
Vonjtech <vonjoeibrahime@gmail.com>
2025-03-13 18:03:38 -0400

    version 1.5

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()], // Include the React plugin
-  server: {
-    port: 3000, // Change this to your desired port number
-  },
-  base: "/"
+  plugins: [react()],
+  base: '/', // Ensure base path is correctly set
 });
